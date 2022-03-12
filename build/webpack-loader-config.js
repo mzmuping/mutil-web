f638797f50a5cf0dadf23bed0bf506580244f319
麦中麦 <824723712@qq.com>
2022-03-12 18:12:13 +0800

    feat

--- build/webpack-loader-config.js
+++ build/webpack-loader-config.js
@@ -1,50 +1,48 @@
-const path = require('path')
-const { appDir } = require('./paths')
-const MyBabelLoader = path.resolve(appDir, 'loader/my-babel-loader.js')
+const path = require('path');
+const { appDir } = require('./paths');
+const MyBabelLoader = path.resolve(appDir, 'loader/my-babel-loader.js');
 module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(process.cwd(), 'dist-loader'),
-        filename: 'main.js',
-    },
-    resolve: {
-        alias: {
-
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                    }
-                ],
-            },
-            {
-                test: /\.(c|le)ss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: MyBabelLoader,
-                        options: {
-                            modules: true
-                        }
-                    },
-                    {
-                        loader: 'less-loader',
-                        options: {
-                            lessOptions: {
-                                strictMath: true,
-                            }
-                        }
-                    }
-                ]
-            }
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(process.cwd(), 'dist-loader'),
+    filename: 'main.js'
+  },
+  resolve: {
+    alias: {}
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
         ]
-    }
-}
+      },
+      {
+        test: /\.(c|le)ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: MyBabelLoader,
+            options: {
+              modules: true
+            }
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                strictMath: true
+              }
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
--- src/mobile/sort/排序算法.js
+++ src/mobile/sort/排序算法.js
@@ -47,7 +47,7 @@ class ArrayList {
 }
 
 const list = new ArrayList();
-
+const a = 1;
 list.insert(6);
 list.insert(5);
 list.insert(3);
