9ba2942bfd7b29336d87f17722ce47a94fe02705
麦中麦 <824723712@qq.com>
2022-04-26 10:46:12 +0800

    feat

--- src/mobile/index/单链表.js
+++ src/mobile/index/单链表.js
@@ -1,46 +1,46 @@
 class LinkeList {
   constructor() {
-    this.head = null
-    this.length = 0
+    this.head = null;
+    this.length = 0;
   }
 
   append(ele) {
-    let newNode = new Node(ele)
+    let newNode = new Node(ele);
     if (this.head === null) {
-      this.head = newNode
+      this.head = newNode;
     } else {
-      let current = this.head
+      let current = this.head;
       while (current.next) {
-        current = current.next
+        current = current.next;
       }
-      current.next = newNode
+      current.next = newNode;
     }
-    this.length += 1
+    this.length += 1;
   }
 
   toString() {
-    let current = this.head
-    let listString = ''
+    let current = this.head;
+    let listString = '';
 
     while (current) {
-      listString += ' ' + current.data.toString()
-      current = current.next
+      listString += ' ' + current.data.toString();
+      current = current.next;
     }
-    return listString
+    return listString;
   }
 
   isEmpty() {
-    return this.length === 0
+    return this.length === 0;
   }
 
   size() {
-    return this.length
+    return this.length;
   }
 }
 
 class Node {
   constructor(data) {
-    this.data = data
-    this.next = null
+    this.data = data;
+    this.next = null;
   }
 }
